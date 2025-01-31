b6a1003686099607c0509f890bd6e68ae2aafd01
t2ggerch0 <chojbsoft@gmail.com>
2025-01-31 15:38:41 +0900

    이전 프레임 충돌 정보에 따른 Begin, Collision, End 호출

--- Engine/CollisionManager.h
+++ Engine/CollisionManager.h
@@ -1,5 +1,15 @@
 #pragma once
+#include "Collider.h"
 
+union COLLIDER_ID
+{
+	struct
+	{
+		UINT left;
+		UINT right;
+	};
+	ULONGLONG ID;
+};
 
 
 class CollisionManager
@@ -21,6 +31,10 @@ private:
 
 	// 충돌체 간의 이전 프레임 충돌 상태
 	// 맵의 키를 위해, 충돌체들은 모두 ID값을 가지고 있을 것
+	// 쌍으로 필요하기 때문에, union으로 두 ID 이어서 키로 사용
+	// 그럼 만약 오브젝트가 파괴됐을 때는 이 정보 어떻게 해제?
+	unordered_map<ULONGLONG, bool> colInfo;
+
 
 };
 
