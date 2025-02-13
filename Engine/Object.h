627c6549159f5229e74f78b2cad4c2353afc4a4c
t2ggerch0 <chojbsoft@gmail.com>
2025-02-13 15:03:40 +0900

    NewObject, Allocator 부분 주석 제거

--- Engine/Object.h
+++ Engine/Object.h
@@ -42,19 +42,13 @@ protected:
 template<class T, class... Args>
 shared_ptr<T> NewObject(Args&&... args)
 {
-	// 처음 생성일 때만
-	// 나중에 allocator 안으로 옮길것
 	const type_info& typeInfo = typeid(T);
 	size_t classSize = sizeof(T);
 	if (!_objectArray.IsExist(typeInfo))
 	{
-		// allocate_shared로 메모리풀에서 연속적으로 관리하기 위해, 레퍼런스카운트 크기 계산
 		const int SharedPtrSize = sizeof(_Ref_count_obj_alloc3 <T, Allocator<T>>) - sizeof(T);
-
-		// 메모리풀 생성
 		_objectArray.Create(typeInfo, SharedPtrSize + classSize);
 	}
 
-	// std::forward로 인자의 참조 타입 유지
 	return allocate_shared<T>(Allocator<T>(typeInfo), std::forward<Args>(args)...);
 }
